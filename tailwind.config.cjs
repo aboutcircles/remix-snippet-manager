190fed2c1df9df4a8ac21807b1ad83266ff99515
daniel <4954577+jaensen@users.noreply.github.com>
2025-09-04 00:31:17 +0200

    initial commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,9 @@
+/******************* Tailwind Config *******************/
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+  theme: {
+    extend: {}
+  },
+  plugins: []
+};
